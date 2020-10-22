e487815c0e9d15a24dd5d2d7214918b9c06813fe
anacarolinacv <ana.vasconcelos@ccc.ufcg.edu.br>
2020-10-22 13:24:54 -0300

    andamento do cadastro

--- codigo/Funcionario.hs
+++ codigo/Funcionario.hs
@@ -5,12 +5,11 @@ import System.Process
 import Control.Monad (when)
 import Text.Printf
  
-type funcionarios = [Funcionario]                                                   
-type nomeFuncionario = String
-type id = String
-type data = String
-type salario = Double
-data Funcionario = Funcionario nomeFuncionario id data salario
+type Funcionarios = [Funcionario]                                                   
+type NomeFuncionario = String
+type Data = String
+type Salario = Double
+data Funcionario = Funcionario nomeFuncionario data salario
 
 
 getNovoNomeDoFuncionario :: IO Double
@@ -29,14 +28,6 @@ getNovoPesoNota = do
    x <- readLn
    return x
 
-getNovoIdDoFuncionario :: IO String
-getNovoNomeDeFuncionario = do 
-   putStrLn("\nDigite um Id do novo funcionário")
-   hSetBuffering stdin LineBuffering
-   hSetEcho stdin True
-   x <- readLn
-   return x
-
 getNovaDataDeAdmissaoDoFuncionario :: IO String
 getNovoNomeDeFuncionario = do 
    putStrLn("\nDigite uma nova Data de admissão do novo funcionário")
@@ -45,6 +36,19 @@ getNovoNomeDeFuncionario = do
    x <- readLn
    return x
 
+--------------------------------------------------------------------------
+changeNomeFuncionario :: Funcionarios -> Integer -> Integer -> String -> Funcionarios
+changeNomeFuncionario [] nomeFuncionario contador novoNomeFuncionario = []
+changeNomeFuncionario ((Funcionario nomeFuncionario Data Salario):os) nomeFuncionario contador novoNomeFuncionario
+   | contador == nomeFuncionario = (Funcionario novoNomeFuncionario Data Salario) : changeNomeFuncionario os nomeFuncionario  (contador+1) novoNomeFuncionario
+   | otherwise = (Funcionario nomeFuncionario Data Salario) : changeNomeFuncionario os nomeFuncionario (contador+1) novoNomeFuncionario
+
+changeAdmissaoFuncionario :: Funcionarios -> Integer -> Integer -> String -> Funcionarios
+changeAdmissaoFuncionario [] nomeFuncionario contador novaDataAdmissao = []
+changeAdmissaoFuncionario ((Funcionario nomeFuncionario Data Salario):os) nomeFuncionario contador novaDataAdmissao
+   | contador == nomeFuncionario = (Funcionario nomeFuncionario novaDataAdmissao Salario) : changeAdmissaoFuncionario os nomeFuncionario  (contador+1) novaDataAdmissao
+   | otherwise = (Funcionario nomeFuncionario Data Salario) : changeAdmissaoFuncionario os nomeFuncionario (contador+1) novaDataAdmissao
+   
 EscreverArquivo :: funcionarios -> IO ()
 EscreverArquivo funcionarios = do
                arq <- openFile "./arquivos/Funcionarios.txt" WriteMode
