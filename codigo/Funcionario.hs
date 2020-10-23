b066f2a915fb6cfebbb9d874be4dbf865cda98cb
anacarolinacv <ana.vasconcelos@ccc.ufcg.edu.br>
2020-10-22 21:48:09 -0300

    a real eh que eu nao sei de nada

--- codigo/Funcionario.hs
+++ codigo/Funcionario.hs
@@ -41,14 +41,20 @@ changeNomeFuncionario :: Funcionarios -> Integer -> Integer -> String -> Funcion
 changeNomeFuncionario [] nomeFuncionario contador novoNomeFuncionario = []
 changeNomeFuncionario ((Funcionario nomeFuncionario Data Salario):os) nomeFuncionario contador novoNomeFuncionario
    | contador == nomeFuncionario = (Funcionario novoNomeFuncionario Data Salario) : changeNomeFuncionario os nomeFuncionario  (contador+1) novoNomeFuncionario
-   | otherwise = (Funcionario nomeFuncionario Data Salario) : changeNomeFuncionario os nomeFuncionario (contador+1) novoNomeFuncionario
+   | otherwise = (Funcionario NomeFuncionario Data Salario) : changeNomeFuncionario os nomeFuncionario (contador+1) novoNomeFuncionario
 
 changeAdmissaoFuncionario :: Funcionarios -> Integer -> Integer -> String -> Funcionarios
 changeAdmissaoFuncionario [] nomeFuncionario contador novaDataAdmissao = []
 changeAdmissaoFuncionario ((Funcionario nomeFuncionario Data Salario):os) nomeFuncionario contador novaDataAdmissao
-   | contador == nomeFuncionario = (Funcionario nomeFuncionario novaDataAdmissao Salario) : changeAdmissaoFuncionario os nomeFuncionario  (contador+1) novaDataAdmissao
-   | otherwise = (Funcionario nomeFuncionario Data Salario) : changeAdmissaoFuncionario os nomeFuncionario (contador+1) novaDataAdmissao
-   
+   | contador == nomeFuncionario = (Funcionario NomeFuncionario novaDataAdmissao Salario) : changeAdmissaoFuncionario os nomeFuncionario  (contador+1) novaDataAdmissao
+   | otherwise = (Funcionario NomeFuncionario Data Salario) : changeAdmissaoFuncionario os nomeFuncionario (contador+1) novaDataAdmissao
+
+changeSalarioDoFuncionario :: Funcionarios -> Integer -> Integer -> Double -> Funcionarios
+changeSalarioDoFuncionario [] nomeFuncionario contador novoSalario = []
+changeSalarioDoFuncionario ((Funcionario nomeFuncionario Data Salario):os) nomeFuncionario contador novoSalario
+   | contador == nomeFuncionario = (Funcionario NomeFuncionario Data novoSalario) : changeSalarioDoFuncionario os nomeFuncionario  (contador+1) novoSalario
+   | otherwise = (Funcionario NomeFuncionario Data Salario) : changeSalarioDoFuncionario os nomeFuncionario (contador+1) novoSalario
+
 EscreverArquivo :: funcionarios -> IO ()
 EscreverArquivo funcionarios = do
                arq <- openFile "./arquivos/Funcionarios.txt" WriteMode
