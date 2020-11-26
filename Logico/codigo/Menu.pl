c45ac4be4521268a8ca5da9202012db0fc54d501
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2020-11-25 22:03:22 -0300

    Estrutura básica das telas feita.

--- Logico/codigo/Menu.pl
+++ Logico/codigo/Menu.pl
@@ -1,7 +1,7 @@
-% :- :- initialization(main).
+:- initialization(main).
 
+/* -> FUNCOES UTEIS -- */
 
-/*------------------------------------------FUNCOES UTEIS----------------------------------------------*/
 up(119).
 down(115).
 left(97).
@@ -9,45 +9,53 @@ right(100).
 select(113).
 remotion(101).
 
+/* -------------------- */
 upAction(0, Limit, Limit).
-upAction(Cursor, _, NewCursor) :- NewCursor is Cursor - 1.
+upAction(Cursor, _, NewCursor) :-
+    NewCursor is Cursor - 1.
 
-downAction(Cursor, Limit, NewCursor) :- Max is Limit + 1,
-                                        Pc is Cursor + 1,
-                                        NewCursor is PC mod Max.
+downAction(Cursor, Limit, NewCursor) :-
+    Max is Limit + 1,
+    PC is Cursor + 1,
+    NewCursor is PC mod Max.
 
-% switch([O|Os], Item, Pos, Cont, NewO) :-
-%     (Pos =:= Cont -> NewO = [Item|Os];
-%         Cont2 is Cont + 1, switch(Os, Item, Pos, Cont2, NewO2), NewO = [O|NewO2]).
+remotionExitAction(Mensagem, Resultado) :-
+    shell(clear),
+    writeln(Mensagem),
+    get_single_char(Input),
+    (remotion(Input) -> Resultado is 1;
+        Resultado is 0).
 
-% remove([], _, _, []).
-% remove([O|Os], Pos, Cont, NewO) :-
-%     (Pos =:= Cont -> NewO = Os;
-%         Cont2 is Cont + 1, remove(Os, Pos, Cont2, NewO2), NewO = [O|NewO2]).
+switch([O|Os], Item, Pos, Cont, NewO) :-
+    (Pos =:= Cont -> NewO = [Item|Os];
+        Cont2 is Cont + 1, switch(Os, Item, Pos, Cont2, NewO2), NewO = [O|NewO2]).
 
-% add([], Item, Item).
-% add([O|Os], Item, NewO) :-
-%     add(Os, Item, NewO2),
-%     NewO = [O|NewO2].
+remove([], _, _, []).
+remove([O|Os], Pos, Cont, NewO) :-
+    (Pos =:= Cont -> NewO = Os;
+        Cont2 is Cont + 1, remove(Os, Pos, Cont2, NewO2), NewO = [O|NewO2]).
+
+add([], Item, Item).
+add([O|Os], Item, NewO) :-
+    add(Os, Item, NewO2),
+    NewO = [O|NewO2].
 
 showOptions([], _, _).
 showOptions([A|As], Cursor, Cursor) :- 
-                            write('-> '),
-                            writeln(A),
-                            N is Cursor + 1,
-                            showOptions(As, Cursor, N).
-                                    
+    write('-> '),
+    writeln(A),
+    N is Cursor + 1,
+    showOptions(As, Cursor, N).
+    
 showOptions([A|As], Cursor, N) :- 
-                            write('   '),
-                            writeln(A),
-                            NewN is N + 1,
-                            showOptions(As, Cursor, NewN).
+    write('   '),
+    writeln(A),
+    NewN is N + 1,
+    showOptions(As, Cursor, NewN).
 
-listNomesDisciplinas([], []).
-listNomesDisciplinas([[Nome|Resto]|Ds], ListaOpcoes) :-
-                    listNomesDisciplinas(Ds, ListaOpcoes2),
-                    ListaOpcoes = [Nome|ListaOpcoes2].
-/*------------------------------------------FUNCOES PARA IO----------------------------------------------*/
+/* FUNCOES UTEIS <- */
+
+/* -> FUNCOES PARA IO */
 
 getString(FinalInput, Mensagem) :-
     write('\n'),
@@ -69,19 +77,20 @@ getInt(FinalInput, Mensagem) :-
     (number_string(Number, Return), Number >= 0 -> FinalInput = Number;
         getDouble(NewF, 'Entrada invalida digite novamente!'), FinalInput is NewF).
 
+/* FUNCOES PARA IO <- */
 
-/*------------------------------------------TELA PRINCIPAL----------------------------------------------*/
+/* -> TELA PRINCIPAL */
 
-optionsMainScreen(['Entrar como gestor', 'Entrar como funcionário', 'Entrar como cliente', 'Sair do sistema']).
-limitMain(4).
+optionsMainScreen(['Entrar como gestor', 'Entrar como funcionario', 'Entrar como cliente', 'Sair']).
+limitMain(3).
 
-/*-------------------------------------------------------------------------------------------------------*/
+/* ------------------------------------------------------------------------------ */
 
 showExitMessage() :-
     shell(clear),
     writeln('---------------------------------------------'),
-    writeln('* OBRIGADO POR UTILIZAR O CORONA PHARM *'),
-    writeln('*                 VOLTE SEMPRE!                 *'),
+    writeln('* MUITO OBRIGADO POR UTILIZAR O CORONA PHARM *'),
+    writeln('*                VOLTE SEMPRE!               *'),
     writeln('---------------------------------------------\n').
 
 doMainScreen(Cursor, Action) :-
@@ -89,15 +98,16 @@ doMainScreen(Cursor, Action) :-
     (up(Action) -> upAction(Cursor, Limit, NewCursor), mainScreen(NewCursor);
      down(Action) -> downAction(Cursor, Limit, NewCursor), mainScreen(NewCursor);
      left(Action) -> showExitMessage();
-     right(Action) -> (Cursor =:= 0 -> write('Você é um gestor.');
-                       Cursor =:= 1 -> write('Você é um funcionário.');
-                       Cursor =:= 2 -> write('Você é um cliente.');
-                       Cursor =:= 3 -> write('Você quer sair do sistema.'));
+     right(Action) -> (Cursor =:= 0 -> masterScreen(0);%acessoDisciplinasScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 1 -> employeeScreen(0);%acessoCompromissosScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 2 -> clientScreen(0);%configuracoesScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 3 -> write('Voce quer sair do sistema'));%tutorialScreen(ListaCompromissos, ListaDisciplinas));
      mainScreen(Cursor)).
 
 mainScreen(Cursor) :-
     shell(clear),
-    writeln('\n|| (w,s) Para mover o cursor  ||'),
+    writeln('\nBEM VINDO À CORONA PHARM!
+             \n|| (w,s) Para mover o cursor  ||'),
     writeln('|| (a) Para modificar a tela  ||'),
     writeln('|| (d) Para entrar nas opcoes ||\n'),
     optionsMainScreen(ListaOpcoes),
@@ -105,7 +115,118 @@ mainScreen(Cursor) :-
     get_single_char(Action),
     doMainScreen(Cursor, Action).
 
-/*--------------------------------------------TELA GESTOR-----------------------------------------------------------*/
+% ---------------------------------------------TELA GESTOR-----------------------------------------------------------
 
-% optionsConfiguracoesScreen(['Cadastrar disciplina', 'Atualizar disciplina', 'Remover disciplina', 'Resetar sistema']).
-% limitGestor(3).
+optionsMasterScreen(['Cadastrar Produto', 'Cadastrar Funcionario', 'Atualizar Preço', 'Visualizar funcionários', 'Visualizar produtos', 'Visualizar clientes', 'Visualizar vendas gerais']).
+limitMaster(6).
+
+doMasterScreen(Cursor, Action) :-
+    limitMaster(Limit),
+    (up(Action) -> upAction(Cursor, Limit, NewCursor), masterScreen(NewCursor);
+     down(Action) -> downAction(Cursor, Limit, NewCursor), masterScreen(NewCursor);
+     left(Action) -> mainScreen(Cursor);
+     right(Action) -> (Cursor =:= 0 -> write('Voce cadastrou um produto');%acessoDisciplinasScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 1 -> write('Voce cadastrou um funcionario');%acessoCompromissosScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 2 -> write('Voce atualizou o preço');%configuracoesScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 3 -> write('Voce visualizou os funcionarios');%tutorialScreen(ListaCompromissos, ListaDisciplinas));
+                       Cursor =:= 4 -> productViewScreen(0);
+                       Cursor =:= 5 -> write('Voce visualizou os clientes');
+                       Cursor =:= 6 -> write('Voce visualizou as vendas'));
+     masterScreen(Cursor)).
+
+masterScreen(Cursor) :-
+    shell(clear),
+    writeln('\nBEM VINDO À CORONA PHARM!
+                \n|| (w,s) Para mover o cursor  ||'),
+    writeln('|| (a) Para modificar a tela  ||'),
+    writeln('|| (d) Para entrar nas opcoes ||\n'),
+    optionsMasterScreen(ListaOpcoes),
+    showOptions(ListaOpcoes, Cursor, 0),
+    get_single_char(Action),
+    doMasterScreen(Cursor, Action).
+
+% --------------------------------------------TELA FUNCIONARIO--------------------------------------------
+optionsEmployeeScreen(['Cadastrar cliente', 'Cadastrar Venda', 'Visualizar clientes', 'Visualizar produtos', 'Visualizar suas vendas']).
+limitEmployee(4).
+
+doEmployeeScreen(Cursor, Action) :-
+    limitEmployee(Limit),
+    (up(Action) -> upAction(Cursor, Limit, NewCursor), employeeScreen(NewCursor);
+     down(Action) -> downAction(Cursor, Limit, NewCursor), employeeScreen(NewCursor);
+     left(Action) -> mainScreen(Cursor);
+     right(Action) -> (Cursor =:= 0 -> write('Voce cadastrou um cliente');%acessoDisciplinasScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 1 -> write('Voce cadastrou uma venda');%acessoCompromissosScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 2 -> write('Voce visualizou os clientes');%configuracoesScreen(ListaCompromissos, ListaDisciplinas, 0);
+                       Cursor =:= 3 -> productViewScreen(0);%tutorialScreen(ListaCompromissos, ListaDisciplinas));
+                       Cursor =:= 4 -> write('Voce visualizou sua lista de vendas'));
+     employeeScreen(Cursor)).
+
+employeeScreen(Cursor) :-
+    shell(clear),
+    writeln('\nBEM VINDO À CORONA PHARM!
+                \n|| (w,s) Para mover o cursor  ||'),
+    writeln('|| (a) Para modificar a tela  ||'),
+    writeln('|| (d) Para entrar nas opcoes ||\n'),
+    optionsEmployeeScreen(ListaOpcoes),
+    showOptions(ListaOpcoes, Cursor, 0),
+    get_single_char(Action),
+    doEmployeeScreen(Cursor, Action).
+
+
+% -----------------------------------------TELA CLIENTE---------------------------------------------------
+
+optionsClientScreen(['Visualizar produtos', 'Comprar produto']).
+limitClient(1).
+
+doClientScreen(Cursor, Action) :-
+    limitClient(Limit),
+    (up(Action) -> upAction(Cursor, Limit, NewCursor), clientScreen(NewCursor);
+     down(Action) -> downAction(Cursor, Limit, NewCursor), clientScreen(NewCursor);
+     left(Action) -> mainScreen(Cursor);
+     right(Action) -> (Cursor =:= 0 -> productViewScreen(0);
+                       Cursor =:= 1 -> write('Voce realizou uma compra'));
+     clientScreen(Cursor)).
+
+clientScreen(Cursor) :-
+    shell(clear),
+    writeln('\nBEM VINDO À CORONA PHARM!
+                \n|| (w,s) Para mover o cursor  ||'),
+    writeln('|| (a) Para modificar a tela  ||'),
+    writeln('|| (d) Para entrar nas opcoes ||\n'),
+    optionsClientScreen(ListaOpcoes),
+    showOptions(ListaOpcoes, Cursor, 0),
+    get_single_char(Action),
+    doClientScreen(Cursor, Action).
+% --------------------------------------Tela Visualizar Produtos------------------------------------------
+
+optionsProductViewScreen(['Visualizar por sintoma', 'Visualizar por existente no estoque']).
+limitProductView(1).
+
+doProductViewScreen(Cursor, Action) :-
+    limitProductView(Limit),
+    (up(Action) -> upAction(Cursor, Limit, NewCursor), productViewScreen(NewCursor);
+     down(Action) -> downAction(Cursor, Limit, NewCursor), productViewScreen(NewCursor);
+     left(Action) -> mainScreen(Cursor);
+     right(Action) -> (Cursor =:= 0 -> write('Voce visualizou os produtos por sintoma');
+                       Cursor =:= 1 -> write('Voce visualizou os produtos por estoque'));
+     productViewScreen(Cursor)).
+
+productViewScreen(Cursor) :-
+    shell(clear),
+    writeln('\nBEM VINDO À CORONA PHARM!
+                \n|| (w,s) Para mover o cursor  ||'),
+    writeln('|| (a) Para modificar a tela  ||'),
+    writeln('|| (d) Para entrar nas opcoes ||\n'),
+    optionsProductViewScreen(ListaOpcoes),
+    showOptions(ListaOpcoes, Cursor, 0),
+    get_single_char(Action),
+    doProductViewScreen(Cursor, Action).
+
+% --------------------------------------------------------------------------------------------------------
+
+
+main :-
+    % readDisciplinas(ListaDisciplinas),
+    % readCompromissos(ListaCompromissos),
+    mainScreen(0),
+    halt(0).
